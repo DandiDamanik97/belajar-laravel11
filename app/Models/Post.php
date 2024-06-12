72a97eb852c110e79f86d8e9ae220b7b6a54fbf4
DandiDamanik <dandidamanik210497@gmail.com>
2024-06-12 10:43:38 +0700

    menambah file poss dan posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,43 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    public static function all () 
-    {
-        return [
-            [
-                'id' => '1',
-                'slug' => 'rantau-artikel-1',
-                'title' => 'Rantau',
-                'author' => 'Dandi Damanik',
-                'body' => 'Setiap orang berhak memilih untuk jalan hidup masing-masing
-                ada pribadi yang untuk tinggal di tanah kelahirannya
-                dan membangun serta memajukan tanah kelahirannya
-                dan ada pribadi yang memutuskan untuk Merantau,
-                dengan tujuan mendapat hidup yang lebih baik'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'proses-artikel-2',
-                'title' => 'Proses',
-                'author' => 'Dandi Damanik',
-                'body' => 'Menikmati setiap proses yang ku jalani,
-                berjuang dan berdoa selama proses perjalanan panjang.
-                dan di 2020 aku menyeslesaikan Sarjana ku'
-            ]
-            ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn ($post) =>$post['slug'] == $slug);
-        
-        if(! $post) {
-            abort(404);
-        }
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable =['title','author','slug','body'];
 }
