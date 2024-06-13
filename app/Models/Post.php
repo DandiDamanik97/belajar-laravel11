71779c462bafcb6b6d581a3d146b609b56fc9579
DandiDamanik <dandidamanik210497@gmail.com>
2024-06-13 14:51:34 +0700

    belajar post categoty

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use PhpParser\Node\Expr\FuncCall;
 
 class Post extends Model
 {
@@ -14,5 +15,10 @@ class Post extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public Function category(): BelongsTo
+    {
+        return $this->belongsTo((Category::class));
     }
 }
